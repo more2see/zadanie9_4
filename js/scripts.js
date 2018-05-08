2df2712f50c3ab4a0b61da35cbcbdd228bb91c61
Dorota Kazak <dorota.kazak@gmail.com>
2018-05-08 21:02:09 +0200

    Change scripts/js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,31 @@
-var treeHeight = 10;
-function drawTree () {
+var treeHeight;
+function drawTree (treeHeight) {
 	for ( i = 0 ; i < treeHeight ; i++) {
-		star = '';
+	  var star = '';
+	  for ( j = 0 ; j < (treeHeight && i) ; j++) {
+		star += '*';
+	  }
+	  console.log(star);
+	}
+}
+drawTree(8);
+
+/*
+2) gdy i == j to break czyli przerwij pętlę. Do tego jest przeznaczone właśnie drugie miejsce w forze. Postaraj się zatem przerobić nieco drugą pętlę.
+*/
+/* Bylo:
+var treeHeight;
+function drawTree (treeHeight) {
+	for ( i = 0 ; i < treeHeight ; i++) {
+		var star = '';
 		for ( j = 0 ; j < treeHeight ; j++) {
 			star += '*';
 	    	if (j == i) {
-        		break;
+        		break; 
     		}
 		}
 	console.log(star);
 	}
 }
-drawTree();
+drawTree(7);
+*/
